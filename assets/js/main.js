a30caca11ac43bd4a50ecafec4352d55b005910d
GianmarcoOrabona <gianmarcoorabona091@gmail.com>
2023-11-27 13:02:13 +0100

    final commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,9 +8,15 @@ let discountCode = ['YHDNU32', 'JANJC63', 'PWKCN25', 'SJDPO96', 'POCIE24',];
 
 let usedDiscountCodes = [];
 
+let showPrice = document.getElementById('price');
+let validation = document.getElementById('validation');
+let usedDiscount = document.getElementById('used_discount');
+
 // Funzione per calcolare il prezzo nel form
 function getPrice(event) {
     event.preventDefault();
+
+    showPrice.innerHTML = '';
 
     // Recupero i dati che mi servono dal form
 
@@ -33,35 +39,56 @@ function getPrice(event) {
     // Controllo se il codice sconto è tra quelli validi
     if (discountCode.includes(inputDiscountCode)) {
         if (usedDiscountCodes.includes(inputDiscountCode)) {
+            usedDiscount.innerHTML = 'Questo codice sconto è già stato utilizzato.'
             console.log('Questo codice sconto è già stato utilizzato.');
 
-            // Esci dalla funzione se il codice è già stato utilizzato
-            return;
+            // Calcolo il prezzo in base al lavoro e alle ore
+            if (typeOfWork == 'backend') {
+                showPrice.innerHTML = `${name} ecco il tuo prezzo ${(backendPrice * hours).toFixed(2)} &euro;`
+                console.log('Ecco il tuo prezzo: ' + (backendPrice * hours).toFixed(2));
+            } else if (typeOfWork == 'frontend') {
+                showPrice.innerHTML = `${name} ecco il tuo prezzo ${(frontendPrice * hours).toFixed(2)} &euro;`
+                console.log('Ecco il tuo prezzo: ' + (frontendPrice * hours).toFixed(2))
+            } else if (typeOfWork == 'analysis') {
+                showPrice.innerHTML = `${name} ecco il tuo prezzo ${(projectAnalysis * hours).toFixed(2)} &euro;`
+                console.log('Ecco il tuo prezzo: ' + (projectAnalysis * hours).toFixed(2))
+            } else {
+                validation.innerHTML = 'Inserisci le ore'
+                console.log('Inserisci il numero di ore');
+            }
         } else {
             // Aggiungi il codice sconto utilizzato
             usedDiscountCodes.push(inputDiscountCode);
             console.log('Sconto applicato.');
 
             // Calcolo il prezzo in base al lavoro e alle ore
-            if (hours && typeOfWork == 'backend') {
+            if (typeOfWork == 'backend') {
+                showPrice.innerHTML = `${name} ecco il tuo prezzo ${((backendPrice * hours * 25) / 100).toFixed(2)} &euro;`
                 console.log('Ecco il tuo prezzo: ' + ((backendPrice * hours * 25) / 100).toFixed(2));
-            } else if (hours && typeOfWork == 'frontend') {
+            } else if (typeOfWork == 'frontend') {
+                showPrice.innerHTML = `${name} ecco il tuo prezzo ${((frontendPrice * hours * 25) / 100).toFixed(2)} &euro;`
                 console.log('Ecco il tuo prezzo: ' + ((frontendPrice * hours * 25) / 100).toFixed(2))
-            } else if (hours && typeOfWork == 'analysis') {
+            } else if (typeOfWork == 'analysis') {
+                showPrice.innerHTML = `${name} ecco il tuo prezzo ${((projectAnalysis * hours * 25) / 100).toFixed(2)} &euro;`
                 console.log('Ecco il tuo prezzo: ' + ((projectAnalysis * hours * 25) / 100).toFixed(2))
             } else {
+                validation.innerHTML = 'Inserisci le ore'
                 console.log('Inserisci il numero di ore');
             }
         }
     } else if (inputDiscountCode == "") {
         // Calcolo il prezzo in base al lavoro e alle ore
-        if (hours && typeOfWork == 'backend') {
+        if (typeOfWork == 'backend') {
+            showPrice.innerHTML = `${name} ecco il tuo prezzo ${(backendPrice * hours).toFixed(2)} &euro;`
             console.log('Ecco il tuo prezzo: ' + (backendPrice * hours).toFixed(2));
-        } else if (hours && typeOfWork == 'frontend') {
+        } else if (typeOfWork == 'frontend') {
+            showPrice.innerHTML = `${name} ecco il tuo prezzo ${(frontendPrice * hours).toFixed(2)} &euro;`
             console.log('Ecco il tuo prezzo: ' + (frontendPrice * hours).toFixed(2))
-        } else if (hours && typeOfWork == 'analysis') {
+        } else if (typeOfWork == 'analysis') {
+            showPrice.innerHTML = `${name} ecco il tuo prezzo ${(projectAnalysis * hours).toFixed(2)} &euro;`
             console.log('Ecco il tuo prezzo: ' + (projectAnalysis * hours).toFixed(2))
         } else {
+            validation.innerHTML = 'Inserisci le ore'
             console.log('Inserisci il numero di ore');
         }
     }
